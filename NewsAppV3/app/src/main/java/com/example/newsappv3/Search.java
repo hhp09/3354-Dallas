5d1523da23181f5e9780814f7106d22cc1b1b6fb
jmm170130 <juanmarquez1979@hotmail.com>
2019-11-21 21:10:25 -0600

    Added Search bar in search page.

--- NewsAppV3/app/src/main/java/com/example/newsappv3/Search.java
+++ NewsAppV3/app/src/main/java/com/example/newsappv3/Search.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
