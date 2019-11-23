2fe42c09206c64052bb7cc60bdd92a6bdf114309
Hritik <hritik81298@gmail.com>
2019-11-22 18:36:42 -0600

    recommitting api files to instantiate api strings and to populate article holder using .xml layout files

--- NewsAppV3/app/src/main/java/com/example/newsappv3/MainActivity.java
+++ NewsAppV3/app/src/main/java/com/example/newsappv3/MainActivity.java
@@ -1,21 +1,60 @@
 package com.example.newsappv3;
 
+// Stores API information, creates objects to store JSON information for each article
+// Storing attribute data for each article in HashMap<String,String>
+// Each key stores JSON value
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String API_KEY = "47da75493074479c95323798e6a853ea"; // secret key
+    ListView listNews;
+    ProgressBar loader;
+
+    // Storing each article in a HashMap as a key,value pair, and the list of articles stored in ArrayList
+    ArrayList<HashMap<String, String>> dataList = new ArrayList<>();
+    static final String KEY_AUTHOR = "author";
+    static final String KEY_TITLE = "title";
+    static final String KEY_DESCRIPTION = "description";
+    static final String KEY_URL = "url";
+    static final String KEY_URLTOIMAGE = "urlToImage";
+    static final String KEY_PUBLISHEDAT = "publishedAt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Home");
+
+        listNews = findViewById(R.id.listNews);
+        loader = findViewById(R.id.loader);
+        listNews.setEmptyView(loader);
+
+        if (SharedFunctions.isNetworkAvailable(getApplicationContext())) {
+            DownloadNews newsTask = new DownloadNews();
+            newsTask.execute();
+        } else {
+            Toast.makeText(getApplicationContext(), "Unable to connect to NewsAPI!", Toast.LENGTH_LONG).show();
+        }
 
         //Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -43,5 +82,58 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    class DownloadNews extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() { super.onPreExecute(); }
+
+        protected String doInBackground(String... args) {
+            // Documentation provides us with endpoint link to contact, we are appending our key to the end of link
+            String xml = SharedFunctions.excuteGet("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY);
+            return xml;
+        }
+
+        @Override
+        protected void onPostExecute(String xml) {
+
+            if (xml.length() > 10) {
+
+                try {
+                    JSONObject jsonResponse = new JSONObject(xml);
+                    JSONArray jsonArray = jsonResponse.optJSONArray("articles");
+
+                    //
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        HashMap<String, String> map = new HashMap<>();
+                        map.put(KEY_AUTHOR, jsonObject.optString(KEY_AUTHOR));
+                        map.put(KEY_TITLE, jsonObject.optString(KEY_TITLE));
+                        map.put(KEY_DESCRIPTION, jsonObject.optString(KEY_DESCRIPTION));
+                        map.put(KEY_URL, jsonObject.optString(KEY_URL));
+                        map.put(KEY_URLTOIMAGE, jsonObject.optString(KEY_URLTOIMAGE));
+                        map.put(KEY_PUBLISHEDAT, jsonObject.optString(KEY_PUBLISHEDAT));
+                        dataList.add(map);
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Unexpected error", Toast.LENGTH_SHORT).show();
+                }
+
+                ArticlePreview adapter = new ArticlePreview(MainActivity.this, dataList);
+                listNews.setAdapter(adapter);
+
+                listNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        Intent i = new Intent(MainActivity.this, DetailsActivity.class);
+                        i.putExtra("url", dataList.get(+position).get(KEY_URL));
+                        startActivity(i);
+                    }
+                });
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Unable to find any articles!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
