d359d4f3be7b08c0aa966aa5ffc39f3e85d3f1e1
imposterFish <51309759+imposterFish@users.noreply.github.com>
2019-11-25 21:15:53 -0600

    Revert "Room"

--- NewsAppV3/app/src/main/java/com/example/newsappv3/SearchArticlePreview.java
+++ NewsAppV3/app/src/main/java/com/example/newsappv3/SearchArticlePreview.java
@@ -5,16 +5,12 @@ package com.example.newsappv3;
 // Each key stores JSON value
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -22,30 +18,16 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 
-public class SearchArticlePreview extends AppCompatActivity {
-
-    String API_KEY = "47da75493074479c95323798e6a853ea";
-    String keyWord;
-    ListView listNews;
-    ProgressBar loader;
-
-    // Storing each article in a HashMap as a key,value pair, and the list of articles stored in ArrayList
-    ArrayList<HashMap<String, String>> dataList = new ArrayList<>();
-    static final String KEY_AUTHOR = "author";
-    static final String KEY_TITLE = "title";
-    static final String KEY_DESCRIPTION = "description";
-    static final String KEY_URL = "url";
-    static final String KEY_URLTOIMAGE = "urlToImage";
-    static final String KEY_PUBLISHEDAT = "publishedAt";
+public class SearchArticlePreview extends NewsDriver {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_article_preview);
+
+        setTitle("");
 
         keyWord = getIntent().getExtras().getString("keyWord");
         listNews = findViewById(R.id.listNews);
@@ -53,7 +35,7 @@ public class SearchArticlePreview extends AppCompatActivity {
         listNews.setEmptyView(loader);
 
         // Download news if network is available
-        if (SharedFunctions.isNetworkAvailable(getApplicationContext())) {
+        if (SharedResources.isNetworkAvailable(getApplicationContext())) {
             DownloadNews newsTask = new DownloadNews();
             newsTask.execute();
         } else {
@@ -89,12 +71,10 @@ public class SearchArticlePreview extends AppCompatActivity {
     }
 
     class DownloadNews extends AsyncTask<String, Void, String> {
-        @Override
-        protected void onPreExecute() { super.onPreExecute(); }
 
         // Perform a Get on the Given URL
         protected String doInBackground(String... args) {
-            String xml = SharedFunctions.excuteGet("https://newsapi.org/v2/everything?" + "q=" + keyWord + "&apiKey=" + API_KEY);
+            String xml = SharedResources.excuteGet("https://newsapi.org/v2/everything?" + "q=" + keyWord + "&apiKey=" + API_KEY);
             return xml;
         }
 
@@ -112,12 +92,13 @@ public class SearchArticlePreview extends AppCompatActivity {
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
                         HashMap<String, String> map = new HashMap<>();
-                        map.put(KEY_AUTHOR, jsonObject.optString(KEY_AUTHOR));
+                        JSONObject source = jsonObject.getJSONObject(KEY_SOURCE);
+                        map.put(KEY_NAME, source.getString(KEY_NAME));
                         map.put(KEY_TITLE, jsonObject.optString(KEY_TITLE));
                         map.put(KEY_DESCRIPTION, jsonObject.optString(KEY_DESCRIPTION));
                         map.put(KEY_URL, jsonObject.optString(KEY_URL));
                         map.put(KEY_URLTOIMAGE, jsonObject.optString(KEY_URLTOIMAGE));
-                        map.put(KEY_PUBLISHEDAT, jsonObject.optString(KEY_PUBLISHEDAT));
+                        map.put(KEY_PUBLISHEDAT, SharedResources.DateFormat(jsonObject.optString(KEY_PUBLISHEDAT)));
                         dataList.add(map);
                     }
                 } catch (JSONException e) {
@@ -125,7 +106,7 @@ public class SearchArticlePreview extends AppCompatActivity {
                 }
 
                 // Populate the Article preview view
-                ArticlePreview adapter = new ArticlePreview(SearchArticlePreview.this, dataList);
+                SharedResources.ArticlePreview adapter = new SharedResources.ArticlePreview(SearchArticlePreview.this, dataList);
                 listNews.setAdapter(adapter);
 
                 // Listener for Article clicks
